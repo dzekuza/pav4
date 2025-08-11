c3a82538ee2c738b8dea450fe574fb83c4765896
dzekuza <dzekuza@gmail.com>
2025-08-11 21:57:53 +0300

    Simplify netlify-server.ts: remove code duplication, reuse main server logic, add @netlify/functions dependency

--- netlify/functions/test-import.js
+++
@@ -1,9 +0,0 @@
-// Test server import
-import('./server.js')
-    .then(module => {
-        console.log('Server import successful:', !!module.createServer);
-    })
-    .catch(error => {
-        console.error('Import error:', error.message);
-        console.error('Stack:', error.stack);
-    }); 
